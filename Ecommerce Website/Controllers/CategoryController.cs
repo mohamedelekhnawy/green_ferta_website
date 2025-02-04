3ad8ee6cd1b4fabefa768573af61def77ac2f4d7
Mohamed Elekhnawy <em55599555@gmail.com>
2025-02-04 19:14:32 +0200

    Add project files.

---
+++ Ecommerce Website/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Website.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+    }
+}
